c2a21ed5605018a68623d5f2d10f09205c7d0aa2
Vladislav Tinovets <cinaviec@gmail.com>
2023-11-07 17:49:46 +0300

    Telegram Bot

--- src/main/java/com/example/spring_booking_bot/Bot.java
+++ src/main/java/com/example/spring_booking_bot/Bot.java
@@ -1,5 +1,6 @@
 package com.example.spring_booking_bot;
 
+import com.example.spring_booking_bot.commands.WorkerCommand;
 import com.example.spring_booking_bot.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,9 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramBot;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
@@ -47,6 +50,15 @@ public class Bot extends TelegramLongPollingBot {
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setKeyboard(Collections.singletonList(k));
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        List<WorkerCommand>workerCommandList = new ArrayList<>();
+
+        for(WorkerCommand w : workerCommandList){
+            if(w.start(update)!=null){
+                sendMessage = w.start(update);
+                break;
+            }
+        }
 
         try{
             execute(sendMessage);
